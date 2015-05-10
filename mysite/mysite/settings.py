5b7ab68eb7c319820fb0297310fb967992f6376f
Ahrar Monsur <ahrarmonsur93@gmail.com>
2015-05-10 10:11:45 -0400

    Gets templates to work

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+TEMPLATE_DIRS = (
+        PROJECT_PATH + '/templates/',
+        )
+print TEMPLATE_DIRS
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -88,6 +92,8 @@ TEMPLATES = [
 	{
 		'BACKEND': 'django.template.backends.django.DjangoTemplates',
 		'DIRS': [os.path.join(BASE_DIR, 'templates')],
+                #'DIRS': ["/home/ahrarmonsur/Dropbox/DB_programming/projects/django_poll/mysite/templates"],
+                
 		'APPS_DIRS': True,
 		'OPTIONS': {
 			'context_processors': ['django.template.context_processors.debug', 'django.template.context_processors.request', 'django.contrib.auth.context_processors.auth',	'django.contrib.messages.context_processors.messages'],
